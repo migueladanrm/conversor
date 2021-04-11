b759f434b044f69bd934d0e073de5b93e059b1bf
Miguel Rivas <migueladanrm@outlook.com>
2021-04-10 22:08:06 -0600

    feat: server setup script

---
+++ scripts/server-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# update os
+sudo apt update && sudo apt upgrade
+
+# install ffmpeg
+sudo apt install ffmpeg
+
+# install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
+
+# docker permissions
+sudo groupadd docker
+
+sudo usermod -aG docker $usermod
+
+newgrp docker
