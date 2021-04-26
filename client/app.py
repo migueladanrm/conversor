fb41611a6fb016b0b3327e85af2c6239ecf1eae6
Miguel Rivas <migueladanrm@outlook.com>
2021-04-26 00:01:48 -0600

    single connection socket

--- client/app.py
+++ client/app.py
@@ -1,11 +1,47 @@
-def read_file(path):
-    stream = b""
+import socket
+import tqdm
+import os
+from argparse import ArgumentParser
+import sys
 
-    file = open(path, "rb")
-    stream = file.read()
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", metavar="file", help="The media file.")
 
-    return stream
+args = parser.parse_args()
 
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 7000
 
-if __name__ == "__main__":
-    print("Hello, world!")
+filename = args.file
+filesize = os.path.getsize(filename)
+
+s = socket.socket()
+
+print(f"[+] Connecting to {SERVER_HOST}:{SERVER_PORT}")
+
+s.connect((SERVER_HOST, SERVER_PORT))
+
+print("[+] Connected.")
+
+# send the filename and filesize
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+# start sending the file
+progress = tqdm.tqdm(range(
+    filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    while True:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
+s.close()
--- server/app.py
+++ server/app.py
@@ -10,6 +10,7 @@ SERVER_PORT = 7000
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 MAX_CONNECTIONS = 10
+TEMP_DIR = "/tmp"
 
 s = socket.socket()
 s.bind((SERVER_HOST, SERVER_PORT))
@@ -20,17 +21,16 @@ print(
 
 client_socket, client_address = s.accept()
 
-print(f"[+] {address} is connected.")
+print(f"[+] {client_address} is connected.")
 
 received = client_socket.recv(BUFFER_SIZE).decode()
 filename, filesize = received.split(SEPARATOR)
 filename = os.path.basename(filename)
 filesize = int(filesize)
 
-
 progress = tqdm.tqdm(range(
     filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as f:
+with open(f"{TEMP_DIR}/{filename}", "wb") as f:
     while True:
         bytes_read = client_socket.recv(BUFFER_SIZE)
         if not bytes_read:
